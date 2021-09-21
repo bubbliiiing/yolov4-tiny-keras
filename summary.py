e58eee7f42a2e2540a2fa26d0e8d46b4fb7db423
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-09-22 00:10:41 +0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -5,7 +5,7 @@ from nets.yolo import yolo_body
 
 if __name__ == "__main__":
     input_shape     = [416, 416, 3]
-    anchors_mask    = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
+    anchors_mask    = [[3, 4, 5], [1, 2, 3]]
     num_classes     = 80
 
     model = yolo_body(input_shape, anchors_mask, num_classes, phi = 0)
